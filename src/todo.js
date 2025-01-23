1571b82fa5887222638bf1946ab7efddefbb053e
Aditya Pant <pantanupam05@gmail.com>
2025-01-23 22:15:28 +0530

    atom family

---
+++ src/todo.js
@@ -0,0 +1,12 @@
+export const TODOS = [
+	{
+		id: 1,
+		title: "Go to gym",
+		description: "hit the gym from 7-9",
+	},
+	{
+		id: 2,
+		title: "Go to eat food",
+		description: "hit the dining table from 7-9",
+	},
+];
